be0f9f3f312b9c7f61caff0522e394cc3c895b4b
w0lf92 <kylemancuso09@gmail.com>
2022-09-03 20:13:10 -0400

    Update client.lua

--- publicTransit/client.lua
+++ publicTransit/client.lua
@@ -1,14 +1,14 @@
-payphones = 
+payphones =
 {
     {
-        marker_position =  
+        marker_position =               -- Waypoint Marker  
         { 
             x = -1080.25390625,
             y = -2575.11572265625,
             z = 13.94453907012939
         },
 
-        taxi_position = 
+        taxi_position =                 -- Taxi Spawn Position
         {
             x = -986.70513916016,
             y = -2458.7255390625,
@@ -18,9 +18,9 @@ payphones =
     }
 }
 
-local vehicleHash = GetHashKey('taxi')
-local driverPedHash = GetHashKey('a_m_m_indian_01')
-RequestModel(vehicleHash)
+local vehicleHash = GetHashKey('taxi')                  -- Taxi Cab Model
+local driverPedHash = GetHashKey('a_m_m_indian_01')     -- Taxi Driver Model
+RequestModel(vehicleHash)                               
 RequestModel(driverPedHash)
 
 Citizen.CreateThread
