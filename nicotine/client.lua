4c5de20a32a5c394676ef1447b1e249c3fb3340a
w0lf92 <kylemancuso09@gmail.com>
2022-09-03 20:02:34 -0400

    Update client.lua

--- nicotine/client.lua
+++ nicotine/client.lua
@@ -1,4 +1,4 @@
-isSmoking = false
+isSmoking = false   -- Boolean switch to show if player is smoking or not
 
 Citizen.CreateThread
 (
@@ -6,27 +6,26 @@ Citizen.CreateThread
         while true do
             Citizen.Wait(1)
 
-            if IsControlJustReleased(1, 174) then
-                if isSmoking == false then
-                    TaskStartScenarioInPlace(GetPlayerPed(-1), "WORLD_HUMAN_SMOKING", 0, true)
-                    isSmoking = true
-                elseif isSmoking == true then
-                    ClearPedTasksImmediately(GetPlayerPed(-1))
-                    isSmoking = false
+            if IsControlJustReleased(1, 174) then                                               -- Check for Release of LeftMouseButton
+                if isSmoking == false then                                                      -- Check if already smoking
+                    TaskStartScenarioInPlace(GetPlayerPed(-1), "WORLD_HUMAN_SMOKING", 0, true)  -- If not, start smoking animation
+                    isSmoking = true                                                            -- Set boolean switch to show player is now smoking
+                elseif isSmoking == true then                                                   -- If so, then cancel the smoking action
+                    ClearPedTasksImmediately(GetPlayerPed(-1))                                  -- Stop all player tasks to kill the animation
+                    isSmoking = false                                                           -- Set boolean switch to show player is no longer smoking
                 end
             end
         end
 
         CreateObject
         (
-            GetHashKey('prop_weed_01'),
-            -1074.1231689453,
-            -2583.5339355469,
-            13.50,
-            --13.665752410889,
-            true,
-            false,
-            false
+            GetHashKey('prop_weed_01'), -- Hash of the prop ID
+            -1074.1231689453,           -- X Coordinate
+            -2583.5339355469,           -- Y Coordinate
+            13.50,                      -- Z Coordinate
+            true,                       -- isNetworkModel (false=client-side/true=server-side)
+            false,                      -- Rockstar Network Model (not sure)
+            false                       -- doorFlag (is this a door?)
         )
     end
 )
